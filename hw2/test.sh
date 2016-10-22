801ab950f2bace2435c5b977dc4a91c9cde33159
PENGChunJen <b00901081@ntu.edu.tw>
2016-10-23 02:16:16 +0800

    modify scripts to check input arguments

--- hw2/test.sh
+++ hw2/test.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-python logistic_regression.py $1 $2 $3
+#!/bin/bash                                                                                                                                             
+if [ "$#" == "3" ]; then
+    python logistic_regression.py $1 $2 $3
+else
+    echo "Usage: ./test.sh <model name> <testing data> <prediction.csv>"
+fi
--- hw2/train.sh
+++ hw2/train.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-python logistic_regression.py $1 $2
+#!/bin/bash                                                                                                                                             
+if [ "$#" == "2" ]; then
+    python logistic_regression.py $1 $2 
+else
+    echo "Usage: ./train.sh <training data> <output model>"
+fi
